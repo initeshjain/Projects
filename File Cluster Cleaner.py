4b667db3973596b6e225b128a3048d9ea4670c0e
Nitesh Jain <47496910+initeshjain@users.noreply.github.com>
2020-05-02 18:30:21 +0530

    Update File Cluster Cleaner.py

--- File Cluster Cleaner.py
+++ File Cluster Cleaner.py
@@ -1,3 +1,13 @@
+##  What is it?  ##
+#This is a automatic file categorisor app, coded in python. 
+#You only need OS Module to run this app
+
+##  How it works?  ##
+#Put this code in a file and save this file with .py extension, 
+#then simply put this file on a folder 
+#where you have lots of file with different extentions.
+
+##  Start Code  ##
 ##  Importing Modules  ##
 import os
 
